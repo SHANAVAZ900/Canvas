8824e00cf4903c8bc5cb9bfab9aad8c23a178e76
zamindar-786 <zamindar900@gmail.com>
2021-10-24 12:15:25 +0530

    Started Sidebar of Canva

--- canvas-project/src/App.js
+++ canvas-project/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header";
+import SideBar from "./Components/SideBar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SideBar />
     </div>
   );
 }
---
+++ canvas-project/src/Components/SideBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../CSS/SideBar.css";
+
+function SideBar() {
+  return (
+    <div>
+      <p>I am sidebar</p>
+    </div>
+  );
+}
+
+export default SideBar;
